d89567eaa6cfd7253ce49ec52f44ce5e31d4e1d6
Lonerr7 <krabik2342@gmail.com>
2021-12-03 18:27:00 +0300

    First commit. Lesson 12. Splitted web app into 3 components: Profile, Navbar, Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        className="header__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Steam_Logo.png"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
