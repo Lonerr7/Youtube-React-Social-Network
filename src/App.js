3045e890094a4683d2fb3a20237acae844867cb6
Lonerr7 <krabik2342@gmail.com>
2021-12-04 14:29:27 +0300

    Lesson 19: Homework. Created and added News.js, Music.js and Settings.js to Router.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Profile from "./components/Profile/Profile";
 
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
   return (
@@ -16,7 +19,10 @@ const App = () => {
         <div className="app-wrapper__content">
           <Routes>
             <Route path="/profile" element={<Profile />} />
-            <Route path="dialogs" element={<Dialogs />} />
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Settings />} />
           </Routes>
         </div>
       </div>
