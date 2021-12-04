2622dbbfe6a2a484d67759eef64a90f243746cb9
Lonerr7 <krabik2342@gmail.com>
2021-12-04 14:22:54 +0300

    Lesson 19. Implemented Router to route Profile and Dialogs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 
 import "./App.css";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper__content">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="dialogs" element={<Dialogs />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
