f9655858c3e87c62ab0c86ee9fd77a2ac42573f4
Lonerr7 <krabik2342@gmail.com>
2021-12-06 19:27:47 +0300

    Lesson 29 Homework. Made some fixes in css files

--- src/redux/state.js
+++ src/redux/state.js
@@ -26,7 +26,6 @@ const state = {
       { id: 1, name: "Vanya", avatar: "https://www.nicepng.com/png/detail/186-1866063_dicks-out-for-harambe-sample-avatar.png" },
       { id: 2, name: "Andrew", avatar: "https://www.nicepng.com/png/detail/186-1866063_dicks-out-for-harambe-sample-avatar.png" },
       { id: 3, name: "Dima", avatar: "https://www.nicepng.com/png/detail/186-1866063_dicks-out-for-harambe-sample-avatar.png" },
-      
     ],
   },
 };
